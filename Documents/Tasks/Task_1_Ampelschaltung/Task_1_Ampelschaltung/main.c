a21b4cac56e971312e51a3d5b73e34df6766acf2
Dennis220999 <43820947+Dennis220999@users.noreply.github.com>
2020-04-24 13:52:18 +0200

    Finish Task 1

--- Documents/Tasks/Task_1_Ampelschaltung/Task_1_Ampelschaltung/main.c
+++ Documents/Tasks/Task_1_Ampelschaltung/Task_1_Ampelschaltung/main.c
@@ -7,36 +7,35 @@
 
 #define F_CPU 16000000UL
 #include <avr/io.h>
-#include <avr/delay.h>
+#include <util/delay.h>
 
 #define TOGGLE_BIT(PORT,BIT) PORT^=(1<<BIT)
 
 void setUp(void){
-	//make DDC0-DDC2 outputs
-	DDRC |= 0b000111;
-	//output DDC0-DDC2 low
-	PORTC &= ~(0b000111);
-		
+	//make DDD2-DDD4 outputs
+	DDRD |= 0b000011100; //0b000011100;
+	//output low
+	PORTD &= ~(0b000011100);		
 }
 void loop(void){
 	while(1){
-		TOGGLE_BIT(DDRC,DDC0); //turn ON RED
+		TOGGLE_BIT(PORTD,DDD2); //turn ON RED
+		_delay_ms(3000);
+		TOGGLE_BIT(PORTD,DDD3); //turn ON YELLOW
 		_delay_ms(1000);
-		TOGGLE_BIT(DDRC,DDC1); //turn ON YELLOW
-		_delay_ms(100);
-		TOGGLE_BIT(DDRC,DDC0);
-		TOGGLE_BIT(DDRC,DDC1); //turn RED & YELLOW OFF
-		TOGGLE_BIT(DDRC,DDC2); //turn ON GREEN
+		TOGGLE_BIT(PORTD,DDD2);
+		TOGGLE_BIT(PORTD,DDD3); //turn RED & YELLOW OFF
+		TOGGLE_BIT(PORTD,DDD4); //turn ON GREEN
+		_delay_ms(3000);
+		TOGGLE_BIT(PORTD,DDD4); //turn OFF GREEN
+		TOGGLE_BIT(PORTD,DDD3); //turn ON YELLOW
 		_delay_ms(1000);
-		TOGGLE_BIT(DDRC,DDC2); //turn OFF GREEN
-		TOGGLE_BIT(DDRC,DDC1); //turn ON YELLOW
-		_delay_ms(100);
-		TOGGLE_BIT(DDRC,DDC1); //turn OFF YELLOW		
+		TOGGLE_BIT(PORTD,DDD3); //turn OFF YELLOW		
 	}
 }
 int main(void)
 {
-	void setUp();
-    void loop();
+	setUp();
+    loop();
 }
 
